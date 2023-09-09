2141fa04f34a91a7a9267e5765e16a29cca60af5
tanlda <ledinhanhtan.stack@gmail.com>
2023-09-09 14:17:17 +0700

    feat: min

---
+++ custom/min.zsh
@@ -0,0 +1,10 @@
+alias sudo="sudo "
+alias g="git"
+alias c="clear"
+alias C="clear"
+alias cc="clear"
+
+alias refb="source ~/.bashrc"
+alias edtb="nano ~/.bashrc"
+
+export PS1="\[\e[0m\](core) \[\e[1;32m\]\u@\h\[\e[9m\]\[\e[0m\]:\[\e[1;34m\]\W\[\e[0m\]$ "
